9d466c077f0fb9845ede268c416eeaa7c8a1b6a1
cemmanuel1 <cemmanuel1@gmail.com>
2013-11-05 13:24:51 -0800

    added username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131105203933) do
+ActiveRecord::Schema.define(:version => 20131105212300) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20131105203933) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
