6a959cc5bc4158dd8d4ae158915c2782acaa3b2a
Laura Montemayor <laumontemayor@gmail.com>
2013-11-05 08:37:07 -0800

    started event id

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -12,6 +12,13 @@ def self.fetch
   JSON.parse(RestClient.get('http://api.meetup.com/2/events.json', params: {sign: true, group_id: "2252591, 3286622", key: '5a67321b745353471a65334647631'}))
 end
 
+def fetch_event_id
+  Calendar.fetch.each do |event|
+    event
+  end
+
+end
+
 #   def self.all
 #     fetch({})
 #   end
@@ -22,4 +29,3 @@ end
   
 
 end
-
