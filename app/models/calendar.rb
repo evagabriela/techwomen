83803d3491a8c6f4d78993739c1de61a5ba83137
Laura Montemayor <laumontemayor@gmail.com>
2013-11-04 21:54:12 -0800

    multiple groups for meet up API

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -9,7 +9,7 @@ def initialize
 end
 
 def self.fetch
-  JSON.parse(RestClient.get('http://api.meetup.com/2/events.json', params: {sign: true, group_id: 2252591, key: '5a67321b745353471a65334647631'} ))
+  JSON.parse(RestClient.get('http://api.meetup.com/2/events.json', params: {sign: true, group_id: "2252591, 3286622", key: '5a67321b745353471a65334647631'}))
 end
 
 #   def self.all
