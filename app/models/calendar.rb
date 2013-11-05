b4f171c93d8aaaa89efc98390e4cbb2ebdae5ccf
Laura Montemayor <laumontemayor@gmail.com>
2013-11-05 09:09:36 -0800

    added date to events

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -4,28 +4,18 @@ class Calendar
 
   attr_reader :attributes
 
-def initialize
-  @attributes = attributes
-end
-
-def self.fetch
-  JSON.parse(RestClient.get('http://api.meetup.com/2/events.json', params: {sign: true, group_id: "2252591, 3286622", key: '5a67321b745353471a65334647631'}))
-end
-
-def fetch_event_id
-  Calendar.fetch.each do |event|
-    event
+  def initialize
+    @attributes = attributes
   end
 
-end
+  def self.fetch
+    JSON.parse(RestClient.get('http://api.meetup.com/2/events.json', params: {sign: true, group_id: "2252591, 3286622", key: '5a67321b745353471a65334647631'}))
+  end
+
 
 #   def self.all
 #     fetch({})
 #   end
 
-#   def self.search_images(search_query)
-#     fetch({"and[category]" => "Images", text: search_query})
-#   end
-  
 
 end
