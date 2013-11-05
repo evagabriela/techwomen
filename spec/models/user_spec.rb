5b2510bb941eecca48df00f947a06bbdc6a1eb1a
cemmanuel1 <cemmanuel1@gmail.com>
2013-11-05 13:03:59 -0800

    removed duplicate files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it { should respond_to(:authenticate) }
+ it { should respond_to(:admin) }
+
+ it { should be_valid }
+ it { should_not be_admin }
+
+ describe "with admin attribute set to 'true'" do
+  before do
+    @user.save!
+    @user.toggle!(:admin)
+  end
+
+  it { should be_admin }
+end
 end
