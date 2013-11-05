6c8cf43d410592c03f6bd2829c54b587dd22a3ca
cemmanuel1 <cemmanuel1@gmail.com>
2013-11-05 14:13:12 -0800

    added admin dashboard shell

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-   def authenticate_admin_user!
-   redirect_to new_user_session_path unless current_user.try(:is_admin?)
+def authenticate_admin_user!
+  authenticate_user!
+  unless current_user.admin?
+    flash[:alert] = "This area is restricted to administrators only."
+    redirect_to root_path
   end
+end
+
+def current_admin_user
+  return nil if user_signed_in? && !current_user.admin?
+  current_user
+end
 end
