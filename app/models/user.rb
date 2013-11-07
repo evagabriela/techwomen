0379771765fa27f7bdde27ce18ec23cfe1b764e9
cemmanuel1 <cemmanuel1@gmail.com>
2013-11-07 11:57:15 -0800

    updated user profile new and show pages with new fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :email, :password, :password_confirmation, presence: true
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :location, :technologies, :blerb, :facebook, :linkedin, :blog, :github, :personal_site, :other_site
   # attr_accessible :title, :body
 end
