9057a27b4bece60b1b57bbb764d8d9f203d75ff1
cemmanuel1 <cemmanuel1@gmail.com>
2013-11-07 12:06:56 -0800

    added company and job title to user profiel

--- db/migrate/20131107191523_add_details_to_users.rb
+++ db/migrate/20131107191523_add_details_to_users.rb
@@ -2,6 +2,8 @@ class AddDetailsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string
     add_column :users, :location, :string
+    add_column :users, :company, :string
+    add_column :users, :job_title, :string
     add_column :users, :technologies, :string
     add_column :users, :blerb, :text
     add_column :users, :facebook, :string
